2511f700bddeaee396e10ff4c7e3c689e616da97
angieardilak <katali1296@gmail.com>
2024-07-12 17:05:43 -0500

    Feat: domain

--- src/main/java/tecnico/domain/domain.java
+++ src/main/java/tecnico/domain/domain.java
@@ -1,5 +1,17 @@
 package tecnico.domain;
 
-public class domain {
+import java.util.Scanner;
 
+public class domain {
+    public static void ingresaDeatalles() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la matricula del avion: ");
+        String matricula = scanner.nextLine();
+
+        System.out.println("Ingrese la capacidad del avión: ");
+        int capacidad = scanner.nextInt();
+        scanner.nextLine();
+        
+    }
 }
