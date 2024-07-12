5ce80df126d428a895be3733f93bebf10eec9173
alejandroSIlgado <alejandrosilgado20@gmail.com>
2024-07-12 15:59:44 -0500

    Feat: Se agrego los menus

---
+++ src/main/java/menus.java
@@ -0,0 +1,71 @@
+public class menus {
+    public static void main(String[] args) {
+        menuTecnico();
+    }
+    public static void menuAdministrador(){
+        System.out.println(" ____  _                           _     _                      \r\n" + //
+                        "| __ )(_) ___ _ ____   _____ _ __ (_) __| | ___                 \r\n" + //
+                        "|  _ \\| |/ _ \\ '_ \\ \\ / / _ \\ '_ \\| |/ _` |/ _ \\                \r\n" + //
+                        "| |_) | |  __/ | | \\ V /  __/ | | | | (_| | (_) |               \r\n" + //
+                        "|____/|_|\\___|_| |_|\\_/ \\___|_| |_|_|\\__,_|\\___/   _            \r\n" + //
+                        "   / \\   __| |_ __ ___ (_)_ __ (_)___| |_ __ _  __| | ___  _ __ \r\n" + //
+                        "  / _ \\ / _` | '_ ` _ \\| | '_ \\| / __| __/ _` |/ _` |/ _ \\| '__|\r\n" + //
+                        " / ___ \\ (_| | | | | | | | | | | \\__ \\ || (_| | (_| | (_) | |   \r\n" + //
+                        "/_/   \\_\\__,_|_| |_| |_|_|_| |_|_|___/\\__\\__,_|\\__,_|\\___/|_|   ");
+        System.out.println(" ");
+        System.out.println("1. Registrar Avion ");
+        System.out.println("2. Asignar Tripulación a Trayecto ");
+        System.out.println("2. Consultar Información de Avión ");
+        System.out.println("2. Consultar Información de Trayecto ");
+        System.out.println("2. Registrar Aeropuerto ");
+        System.out.println("2. Asignar Tripulación a Trayecto ");
+        System.out.println("2. Asignar Tripulación a Trayecto ");
+        System.out.println("2. Asignar Tripulación a Trayecto ");
+
+
+    }
+    public static void menuAgente(){
+        System.out.println(" ____  _                           _     _       \r\n" + //
+                        "| __ )(_) ___ _ ____   _____ _ __ (_) __| | ___  \r\n" + //
+                        "|  _ \\| |/ _ \\ '_ \\ \\ / / _ \\ '_ \\| |/ _` |/ _ \\ \r\n" + //
+                        "| |_) | |  __/ | | \\ V /  __/ | | | | (_| | (_) |\r\n" + //
+                        "|____/|_|\\___|_| |_|\\_/ \\___|_| |_|_|\\__,_|\\___/ \r\n" + //
+                        "   / \\   __ _  ___ _ __ | |_ ___                 \r\n" + //
+                        "  / _ \\ / _` |/ _ \\ '_ \\| __/ _ \\                \r\n" + //
+                        " / ___ \\ (_| |  __/ | | | ||  __/                \r\n" + //
+                        "/_/   \\_\\__, |\\___|_| |_|\\__\\___|                \r\n" + //
+                        "        |___/             ");
+                        System.out.println(" ");
+                        System.out.println("1. Registrar Avion ");
+                        System.out.println("2. Asignar Tripulación a Trayecto ");
+                
+    }
+    public static void menuCliente(){
+        System.out.println(" ____  _                           _     _       \r\n" + //
+                        "| __ )(_) ___ _ ____   _____ _ __ (_) __| | ___  \r\n" + //
+                        "|  _ \\| |/ _ \\ '_ \\ \\ / / _ \\ '_ \\| |/ _` |/ _ \\ \r\n" + //
+                        "| |_) | |  __/ | | \\ V /  __/ | | | | (_| | (_) |\r\n" + //
+                        "|____/|_|\\___|_| |_|\\_/ \\___|_| |_|_|\\__,_|\\___/ \r\n" + //
+                        " / ___| (_) ___ _ __ | |_ ___                    \r\n" + //
+                        "| |   | | |/ _ \\ '_ \\| __/ _ \\                   \r\n" + //
+                        "| |___| | |  __/ | | | ||  __/                   \r\n" + //
+                        " \\____|_|_|\\___|_| |_|\\__\\___|   ");
+                        System.out.println(" ");
+                        System.out.println("1. Registrar Avion ");
+                        System.out.println("2. Asignar Tripulación a Trayecto ");
+    }
+    public static void menuTecnico(){
+        System.out.println(" ____  _                           _     _       \r\n" + //
+                        "| __ )(_) ___ _ ____   _____ _ __ (_) __| | ___  \r\n" + //
+                        "|  _ \\| |/ _ \\ '_ \\ \\ / / _ \\ '_ \\| |/ _` |/ _ \\ \r\n" + //
+                        "| |_) | |  __/ | | \\ V /  __/ | | | | (_| | (_) |\r\n" + //
+                        "|____/|_|\\___|_| |_|\\_/ \\___|_| |_|_|\\__,_|\\___/ \r\n" + //
+                        "|_   _|__  ___ _ __ (_) ___ ___                  \r\n" + //
+                        "  | |/ _ \\/ __| '_ \\| |/ __/ _ \\                 \r\n" + //
+                        "  | |  __/ (__| | | | | (_| (_) |                \r\n" + //
+                        "  |_|\\___|\\___|_| |_|_|\\___\\___/ ");
+                        System.out.println(" ");
+                        System.out.println("1. Registrar Avion ");
+                        System.out.println("2. Asignar Tripulación a Trayecto ");
+    }
+}
